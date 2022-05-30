c5d08fb2eb3e04bf03805ac2a7c1e0f2039e8a5e
jpalanca <jpalanca@gmail.com>
2022-05-30 08:32:25 +0000

    added analytics

--- point_and_clickle/games/admin.py
+++ point_and_clickle/games/admin.py
@@ -5,7 +5,9 @@ from point_and_clickle.games.models import Game, DailyGame
 
 class GameAdmin(admin.ModelAdmin):
     search_fields = ['title']
+    ordering = ('title',)
     list_filter = ('is_pointandclick', 'is_valid')
+    list_display = ('title', 'control', 'is_pointandclick', 'is_valid')
 
 admin.site.register(Game, GameAdmin)
 
