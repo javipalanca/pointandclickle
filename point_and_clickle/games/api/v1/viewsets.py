db9a3b3f38bd6dabec48309c79ecab43cf5a7298
jpalanca <jpalanca@gmail.com>
2022-06-02 01:31:44 +0200

    Only add hits to today's game.

--- point_and_clickle/games/api/v1/viewsets.py
+++ point_and_clickle/games/api/v1/viewsets.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import action
@@ -6,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from point_and_clickle.games.api.v1.serializers import GameSerializer
-from point_and_clickle.games.models import Game
+from point_and_clickle.games.models import Game, DailyGame
 
 
 class GameViewSet(RetrieveModelMixin, GenericViewSet):
@@ -19,7 +21,9 @@ class GameViewSet(RetrieveModelMixin, GenericViewSet):
     def hit(self, request, id):
         game = Game.objects.get(id=id)
         guess = request.data.get('hit')
-        game.add_hit(guess)
+        todays_game = DailyGame.objects.get(date=date.today())
+        if todays_game.id == game.id:
+            game.add_hit(guess)
         return Response(status=status.HTTP_200_OK)
 
     @action(detail=True, methods=['get'])
